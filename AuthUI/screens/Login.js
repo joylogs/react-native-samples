8eeafe8aeb22b67a5532ab99126b18e147a98b5c
Joy <joy.logs@gmail.com>
2024-05-20 22:29:08 +0530

    Adding Login, Signup Screens

---
+++ AuthUI/screens/Login.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native'
+import { Input, NativeBaseProvider, Button, Icon, Box, Image, AspectRatio } from 'native-base'
+import { FontAwesome5 } from '@expo/vector-icons'
+import { useNavigation } from '@react-navigation/native'
+
+
